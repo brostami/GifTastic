4fb7ff5312584ec3a761abff2e1771de64ca9373
brostami <brostami@users.noreply.github.com>
2017-06-26 01:14:04 -0400

    Added app functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,27 @@
 $(document).ready(function () {
 var animals = ["cat", "dog", "squirrel", "rabbit", "fox", "gopher", "hamster", "hedgehog"];
 
-// Creates initial array buttons
+loadBtns();
+
+// Create initial array buttons
 function loadBtns() {
     for (var i = 0; i < animals.length; i++) {
         createBtn(animals[i]);
     }
 }
 
-// Creates buttons
+// Create buttons
 function createBtn(animal) {
-    var button = $("<button style='margin-right: 5px;'>");
+    var button = $("<button style='margin: 0 5px 5px 0;'>");
     button.addClass("btn btn-primary animalBtn");
     button.attr("data-name", animal);
     button.text(animal);
     $("#animalBtns").append(button);
 }
 
+// Add click event listener to animal buttons to get gifs from giphy api and display in #gifs div
 $("#animalBtns").on("click", ".animalBtn", function() {
+    $("#gifs").empty();
     var clickedAnimal = $(this).text();
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + clickedAnimal + "&api_key=dc6zaTOxFJmzC&limit=10";
     $.ajax({
@@ -29,7 +33,11 @@ $("#animalBtns").on("click", ".animalBtn", function() {
             var animalDiv = $("<div style='display: inline-block; margin: 0 5px 20px 0;'>");
             var rating = $("<p>").text("Rating: " + results[j].rating.toUpperCase());
             var animalImg = $("<img>");
-            animalImg.attr("src", results[j].images.fixed_height.url);
+            animalImg.attr("src", results[j].images.fixed_height_still.url);
+            animalImg.attr("data-still", results[j].images.fixed_height_still.url);
+            animalImg.attr("data-animate", results[j].images.fixed_height.url);
+            animalImg.attr("data-state", "still");
+            animalImg.addClass("gif");
             animalDiv.append(rating);
             animalDiv.append(animalImg);
             $("#gifs").append(animalDiv);
@@ -37,6 +45,24 @@ $("#animalBtns").on("click", ".animalBtn", function() {
     });
 });
 
-loadBtns();
+$("#submitBtn").on("click", function(event) {
+    event.preventDefault();
+    var newAnimal = $("#newAnimal").val().trim();
+    $("#newAnimal").val("");
+    animals.push(newAnimal);
+    createBtn(newAnimal);
+});
+
+$("#gifs").on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    }
+    else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
 
 });
